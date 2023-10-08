678b86f71ddd473f7013d0111104ad555bc582a2
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-08 21:49:12 +0200

    Create articles table

---
+++ db/schema.rb
@@ -0,0 +1,10 @@
+ActiveRecord::Schema.define(version: 2023_10_08_193452) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
