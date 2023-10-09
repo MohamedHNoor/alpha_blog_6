743e4feaa14ca31436d0ae1a84916ae506f5007b
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-09 16:23:57 +0200

    Create edit and update functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,38 @@ class ArticlesController < ApplicationController
  def show
   @article = Article.find(params[:id])
  end
+
+ def new
+  @article = Article.new
+ end
+
+ def edit
+  @article = Article.find(params[:id])
+ end
+
+ def create
+  @article = Article.new(article_params)
+  if @article.save
+   flash[:notice] = "Article was created successfully."
+   redirect_to @article
+  else
+   render 'new'
+  end
+ end
+
+ def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+   flash[:notice] = "Article was updated successfully."
+   redirect_to @article
+  else
+   render 'edit'
+  end
+ end
+
+ private
+
+ def article_params
+  params.require(:article).permit(:title, :description)
+ end
 end
