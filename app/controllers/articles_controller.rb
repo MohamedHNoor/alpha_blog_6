c4e2d1ee3bbecba1962407234f93c252777a8618
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-21 13:32:00 +0200

    Add user index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
  before_action :set_article, only: [:show, :edit, :update, :destroy]
  
  def index
-  @article = Article.all
+  @articles = Article.all
  end
 
  def show
