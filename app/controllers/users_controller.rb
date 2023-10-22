005241266ef4b51600707876795c714b7812aa70
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-22 12:33:45 +0200

    Add authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 
+ before_action :set_user, only: [:show, :edit, :update]
+
  def index
   @users = User.paginate(page: params[:page], per_page: 5)
  end
 
  def show 
-  @user = User.find(params[:id])
   @articles = @user.articles.paginate(page: params[:page], per_page: 2)
  end
 
@@ -16,6 +17,7 @@ class UsersController < ApplicationController
  def create
   @user = User.new(user_params)
   if @user.save
+   session[:user_id] = @user.id
    flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up."
    redirect_to articles_path
   else
@@ -24,11 +26,11 @@ class UsersController < ApplicationController
  end
 
  def edit 
-  @user = User.find(params[:id])
+  
  end
 
  def update
-  @user = User.find(params[:id])
+  
   if @user.update(user_params)
    flash[:notice] = "Your account information was successfully updated."
    redirect_to @user
@@ -39,7 +41,12 @@ class UsersController < ApplicationController
 
  private
 
+ def set_user
+  @user = User.find(params[:id])
+ end
+
  def user_params
   params.require(:user).permit(:username, :email, :password)
  end
+
 end
