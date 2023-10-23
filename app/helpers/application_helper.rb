5b8689fee550f9bdca91522758af30ce0e39900b
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-23 13:11:02 +0200

    Update controller methods and helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,13 +7,5 @@ module ApplicationHelper
   gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
   image_tag(gravatar_url, alt: user.username, class: "rounded mx-auto d-block shadow mt-4")
  end
-
- def current_user
-  @current_user ||= User.find(session[:user_id]) if session[:user_id]
- end
-
- def logged_in?
-  !!current_user
- end
  
 end
