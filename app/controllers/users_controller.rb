8dd55a672dc2af49217e8d891470b7ea7ddd5c9e
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-10-23 14:58:51 +0200

    Add delete user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
- before_action :set_user, only: [:show, :edit, :update]
- before_action :require_user, only: [:edit, :update]
+ before_action :set_user, only: [:show, :edit, :update, :destroy]
+ before_action :require_user, only: [:edit, :update, :destroy]
 
  def index
   @users = User.paginate(page: params[:page], per_page: 5)
@@ -27,17 +27,22 @@ class UsersController < ApplicationController
  end
 
  def edit 
-  
  end
 
  def update
-  
   if @user.update(user_params)
    flash[:notice] = "Your account information was successfully updated."
    redirect_to @user
   else
    render 'edit'
   end
+ end
+
+ def destroy
+  @user.destroy
+  session[:user_id] = nil
+  flash[:notice] = "Account and all associated articles successfully deleted."
+  redirect_to articles_path
  end
 
  private
